1c8542e2ba5dee1582da5fe31a4d2b501d5c12a8
Ben Sieradzki <benjamin_sieradzki@student.uml.edu>
2023-03-17 11:32:24 -0400

    more fix

--- acceptance_test_8_1.sh
+++ acceptance_test_8_1.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 test $(curl calculator-service:8080/sum?a=1\&b=2) -eq 3
+test $(curl calculator-service:8080/div?a=10\&b=2) -eq 5
+test $(curl calculator-service:8080/div?a=10\&b=0) -eq 5
